fbf0c2495a785c5f8086378d5d13bceabc4c3d66
danicg06work <danicg06trabajo@gmail.com>
2025-12-09 16:49:47 +0100

    Arreglo alguna cosilla que otra y añado css (estilo) a las view

--- src/main/java/com/danielcazalilla/tiendaCamisetas/configuration/SecurityConfiguration.java
+++ src/main/java/com/danielcazalilla/tiendaCamisetas/configuration/SecurityConfiguration.java
@@ -16,62 +16,52 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-      @Autowired 
-      DataSource dataSource;
-      
-      @Autowired
-      public void configure(AuthenticationManagerBuilder amb) throws Exception {
-        amb.jdbcAuthentication()
-            .dataSource(dataSource)
-            .usersByUsernameQuery("select username, password, enabled "+
-                "from usuario where username = ?")
-            .authoritiesByUsernameQuery("select u.username, r.rol as 'authority' "+
-                "from usuario u, rol_usuario r " +
-                "where u.id=r.usuario_id and username = ?");
-      }
+        @Autowired
+        DataSource dataSource;
 
-      @Bean
-      BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-      }
+        @Autowired
+        public void configure(AuthenticationManagerBuilder amb) throws Exception {
+                amb.jdbcAuthentication()
+                                .dataSource(dataSource)
+                                .usersByUsernameQuery(
+                                                "select username, password, enabled from usuario where username = ?")
+                                .authoritiesByUsernameQuery(
+                                                "select u.username, r.rol as 'authority' " +
+                                                                "from usuario u, rol_usuario r " +
+                                                                "where u.id=r.usuario_id and username = ?");
+        }
 
-      @Bean
+        @Bean
+        BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
+        @Bean
         public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-                
-                
-
                 return http
-                        .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/webjars/**", "/img/**", "/js/**", 
-                                        "/register/**", "/ayuda/**", "/login", "/codpos/**", 
-                                        "/denegado", "/error", "/acerca")
-                                .permitAll() 
-                                .requestMatchers("/admin/**", "/admin/*/**" , "/admin/*/*/**", "/admin/*/*/*/*/*/**")
-                                //.authenticated()
-                                .hasAuthority("GESTOR")
-                                .requestMatchers("/pedidos/**", "/pedidos/*/**", "/pedidos/*/*/**", "/pedidos/*/*/*/**", "/pedidos/*/*/*/*/**")
-                                //.authenticated()
-                                .hasAuthority("OPERARIO")
-                                .requestMatchers("/mis-pedidos/**", "/mis-pedidos/*/**", 
-                                    "/productos/**", "/productos/*/**",
-                                    "/mis-datos/**", "/mis-datos/*/**","/mis-datos/*/*/**","/mis-datos/*/*/*/**",
-                                    "/carro/**", "/carro/*/**")
-                                //.authenticated()
-                                .hasAuthority("CLIENTE")
-                        ).exceptionHandling((exception)-> exception.
-                                accessDeniedPage("/denegado") )
-                        .formLogin((formLogin) -> formLogin
-                                .loginPage("/login")
-                                .permitAll()
-                        ).rememberMe(
-                                Customizer.withDefaults()
-                        ).logout((logout) -> logout
-                                .invalidateHttpSession(true)
-                                .logoutSuccessUrl("/")
-                                .permitAll()                                
-                        ).csrf((protection) -> protection
-                                 .disable()    
-                        ).build();
+                                .authorizeHttpRequests((requests) -> requests
+                                                // Recursos estáticos
+                                                .requestMatchers("/css/**", "/js/**", "/img/**", "/webjars/**")
+                                                .permitAll()
 
+                                                // Páginas públicas
+                                                .requestMatchers("/", "/index", "/productos/**", "/ayuda", "/acerca",
+                                                                "/login", "/register", "/error", "/denegado")
+                                                .permitAll()
+
+                                                // Rutas que requieren estar logueado
+                                                .requestMatchers("/carro/**", "/mis-pedidos/**", "/mis-datos/**",
+                                                                "/pedidos/**", "/admin/**")
+                                                .authenticated())
+                                .exceptionHandling((exception) -> exception.accessDeniedPage("/denegado"))
+                                .formLogin((formLogin) -> formLogin
+                                                .loginPage("/login")
+                                                .permitAll())
+                                .logout((logout) -> logout
+                                                .invalidateHttpSession(true)
+                                                .logoutSuccessUrl("/")
+                                                .permitAll())
+                                .csrf((csrf) -> csrf.disable())
+                                .build();
         }
 }
